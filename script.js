9977e3a5fcced861ec58d87111800bbe3edfe4a9
mina <rmaomina@gmail.com>
2022-07-19 14:43:37 +0900

    좋아요 기능 추가

--- script.js
+++ script.js
@@ -1,46 +1,57 @@
-const ul = document.querySelector("ul.discussions__container");
-const submitBtn = document.querySelector(".form__submit")
+const ul = document.querySelector('ul.discussions__container');
+const form = document.querySelector('form.form')
 
+// LocalStorage에 객체를 저장합니다. 
 const saveDataLocalStorage = (obj) => {
   localStorage.setItem('agoraData', JSON.stringify(obj));
   return;
 }
 
+// LocalStorage에서 객체를 가져옵니다. 
 const getDataLocalStorage = (name) => {
   let localData = JSON.parse(localStorage.getItem(name))
   return localData;
 }
 
+// 객체를 받아 Discussion HTML을 작성합니다.
 const convertToDiscussion = (obj) => {
-  const li = document.createElement("li"); 
-  li.className = "discussion__container";
+  const li = document.createElement('li'); 
+  li.className = 'discussion__container';
 
-  const avatarWrapper = document.createElement("div");
-  avatarWrapper.className = "discussion__avatar--wrapper";
-  const discussionContent = document.createElement("div");
-  discussionContent.className = "discussion__content";
-  const discussionAnswered = document.createElement("div");
-  discussionAnswered.className = "discussion__answered";
+  const avatarWrapper = document.createElement('div');
+  avatarWrapper.className = 'discussion__avatar--wrapper';
+  const discussionContent = document.createElement('div');
+  discussionContent.className = 'discussion__content';
+  const discussionAnswered = document.createElement('div');
+  discussionAnswered.className = 'discussion__answered';
 
-  const avatarImage = document.createElement("img");
+  const discussionLikes = document.createElement('div')
+  discussionLikes.classList.add('discussion__likes')
+  discussionLikes.addEventListener('click', likesEventHandler)
+  avatarWrapper.append(discussionLikes)
+
+  // 좋아요 기능 -> 좋아요가 0개 이상이라도 있으면 checked
+  obj.likes > 0?discussionLikes.textContent = obj.likes:discussionLikes.textContent = 0
+
+  const avatarImage = document.createElement('img');
   avatarImage.classList.add('discussion__avatar--image')
   avatarImage.src = obj.avatarUrl;
   avatarImage.alt = `avatar of ${obj.author}`
   avatarWrapper.append(avatarImage)
 
-  const discussionTitle = document.createElement("h2")
+  const discussionTitle = document.createElement('h2')
   discussionTitle.classList.add('discussion__title')
-  const discussionAnchor = document.createElement("a")
+  const discussionAnchor = document.createElement('a')
   discussionAnchor.href = obj.url
   discussionAnchor.textContent = obj.title
   discussionTitle.append(discussionAnchor)
 
-  const discussionInfo = document.createElement("div")
+  const discussionInfo = document.createElement('div')
   discussionInfo.textContent = `${obj.author} | ${obj.createdAt}`
   discussionInfo.classList.add('discussion__information')
   discussionContent.append(discussionTitle, discussionInfo)
 
-  const discussionChecked = document.createElement("p")
+  const discussionChecked = document.createElement('p')
   discussionAnswered.textContent = '☑'
   discussionAnswered.append(discussionChecked)
 
@@ -49,6 +60,7 @@ const convertToDiscussion = (obj) => {
   return li;
 };
 
+// element에 추가합니다. 
 const render = (element) => {
   ul.innerHTML = ''
   let localData = getDataLocalStorage('agoraData')
@@ -58,34 +70,27 @@ const render = (element) => {
   return;
 };
 
-// submit button click시 로컬 스토리지에 추가 (create)
-const createAgoraDiscussion = () => {
-  let userName = document.querySelector("#name").value
-  let userTitle = document.querySelector("#title").value
-  let userStory = document.querySelector("#story").value
+// Submit button click시 discussion 객체를 생성합니다.
+const createAgoraDiscussion = (event) => {
+  event.preventDefault()
+
+  let userName = document.querySelector('#name').value
+  let userTitle = document.querySelector('#title').value
+  let userStory = document.querySelector('#story').value
 
   if (userName && userTitle && userStory) {
     let date = new Date
     let createDate = date.toLocaleString()
     let newObj = {
-      id: "D_kwDOHOApLM4APjJi",
+      id: 'dummy_ID',
       createdAt: createDate,
       title: userTitle,
-      url: "https://github.com/codestates-seb/agora-states-fe/discussions/45",
+      url: 'https://github.com/codestates-seb/agora-states-fe/discussions/45',
       author: userName,
-      answer: {
-        id: "DC_kwDOHOApLM4AKg6M",
-        createdAt: createDate,
-        url: "https://github.com/codestates-seb/agora-states-fe/discussions/45#discussioncomment-2756236",
-        author: userName,
-        bodyHTML:
-          '<p dir="auto">안녕하세요. <a class="user-mention notranslate" data-hovercard-type="user" data-hovercard-url="/users/dubipy/hovercard" data-octo-click="hovercard-link-click" data-octo-dimensions="link_type:self" href="https://github.com/dubipy">@dubipy</a> 님!<br>\n코드스테이츠 교육 엔지니어 권준혁 입니다. <g-emoji class="g-emoji" alias="raised_hands" fallback-src="https://github.githubassets.com/images/icons/emoji/unicode/1f64c.png">🙌</g-emoji></p>\n<p dir="auto">질문 주신 내용은 노드 환경이 구성되어 있지 않기 때문에 발생되는 문제로 확인됩니다.</p>\n<p dir="auto"><code class="notranslate">brew unlink node &amp;&amp; brew link node</code></p>\n<p dir="auto">노드를 연결해 보시고 안된다면</p>\n<p dir="auto"><code class="notranslate">brew link --overwrite node</code></p>\n<p dir="auto">이 명령어를 그 다음에도 안된다면 접근권한 문제일 가능성이 큽니다.</p>\n<p dir="auto"><code class="notranslate">$ sudo chmod 776 /usr/local/lib</code> 접근 권한 변경 후<br>\n<code class="notranslate">$ brew link --overwrite node</code> 다시 연결을 해보세요 !</p>\n<p dir="auto">그럼에도 안된다면 다시 한 번 더 질문을 남겨주세요 !</p>\n<p dir="auto">답변이 되셨다면 내용을 간략하게 정리해서 코멘트를 남기고 answered를 마크해주세요 <g-emoji class="g-emoji" alias="white_check_mark" fallback-src="https://github.githubassets.com/images/icons/emoji/unicode/2705.png">✅</g-emoji><br>\n감사합니다.<g-emoji class="g-emoji" alias="rocket" fallback-src="https://github.githubassets.com/images/icons/emoji/unicode/1f680.png">🚀</g-emoji><br>\n코드스테이츠 교육 엔지니어 권준혁</p>',
-        avatarUrl: "https://avatars.githubusercontent.com/u/79903256?s=64&v=4",
-      },
-      bodyHTML:
-        '<p dir="auto">--------------- 여기서부터 복사하세요 ---------------</p>\n<p dir="auto">운영 체제: 예) macOS</p>\n<p dir="auto">현재 어떤 챕터/연습문제/과제를 진행 중이고, 어떤 문제에 부딪혔나요?<br>\nPair 과제 / JavaScript Koans</p>\n<p dir="auto">npm install 명령어 입력 시 env: node: No such file or directory 라고 뜹니다</p>\n<p dir="auto">에러 발생하여 아래 명령어 실행 했는데도 불구하고 똑같은 에러가 발생했습니다<br>\nnpm cache clean --force</p>\n<p dir="auto">rm package-lock.json</p>\n<p dir="auto">rm -rf ./node_modules/</p>\n<p dir="auto">npm --verbose install</p>\n<p dir="auto">폴더 자체가 문제가 있다고 생각하여 github에서 다시 fork 후 진행했는데도 같은 에러가 발생했습니다<br>\n리눅스 기초 챕터 때 npm 설치해서 마지막 submit까지는 잘 됐는데 현재 짝수 생성기 폴더도 똑같이 npm install 시 no such file or directory가 발생합니다</p>\n<p dir="auto">에러가 출력된 곳에서, 이유라고 생각하는 부분을 열 줄 이내로 붙여넣기 해 주세요. (잘 모르겠으면 에러라고 생각하는 곳을 넣어주세요)</p>\n<div class="highlight highlight-source-js position-relative overflow-auto" data-snippet-clipboard-copy-content="minjun@dubi fe-sprint-javascript-koans-main % pwd \n/Users/minjun/Documents/fe_frontand_39/fe-sprint-javascript-koans-main\nminjun@dubi fe-sprint-javascript-koans-main % npm install \nenv: node: No such file or directory"><pre><span class="pl-s1">minjun</span>@<span class="pl-s1">dubi</span> <span class="pl-s1">fe</span><span class="pl-c1">-</span><span class="pl-s1">sprint</span><span class="pl-c1">-</span><span class="pl-s1">javascript</span><span class="pl-c1">-</span><span class="pl-s1">koans</span><span class="pl-c1">-</span><span class="pl-s1">main</span> <span class="pl-c1">%</span> <span class="pl-s1">pwd</span> \n<span class="pl-c1">/</span><span class="pl-v">Users</span><span class="pl-c1">/</span><span class="pl-s1">minjun</span><span class="pl-c1">/</span><span class="pl-v">Documents</span><span class="pl-c1">/</span><span class="pl-s1">fe_frontand_39</span><span class="pl-c1">/</span><span class="pl-s1">fe</span><span class="pl-c1">-</span><span class="pl-s1">sprint</span><span class="pl-c1">-</span><span class="pl-s1">javascript</span><span class="pl-c1">-</span><span class="pl-s1">koans</span><span class="pl-c1">-</span><span class="pl-s1">main</span>\n<span class="pl-s1">minjun</span><span class="pl-kos"></span>@<span class="pl-s1">dubi</span> <span class="pl-s1">fe</span><span class="pl-c1">-</span><span class="pl-s1">sprint</span><span class="pl-c1">-</span><span class="pl-s1">javascript</span><span class="pl-c1">-</span><span class="pl-s1">koans</span><span class="pl-c1">-</span><span class="pl-s1">main</span> <span class="pl-c1">%</span> <span class="pl-s1">npm</span> <span class="pl-s1">install</span> \nenv: node: <span class="pl-v">No</span> <span class="pl-s1">such</span> <span class="pl-s1">file</span> <span class="pl-s1">or</span> <span class="pl-s1">directory</span></pre></div>\n<p dir="auto">검색했던 링크가 있다면 첨부해 주세요.<br>\n<a href="https://mia-dahae.tistory.com/89" rel="nofollow">https://mia-dahae.tistory.com/89</a></p>\n<p dir="auto"><a href="https://stackoverflow.com/questions/38143558/npm-install-resulting-in-enoent-no-such-file-or-directory" rel="nofollow">https://stackoverflow.com/questions/38143558/npm-install-resulting-in-enoent-no-such-file-or-directory</a></p>\n<p dir="auto"><a href="https://velog.io/@hn04147/npm-install-%ED%95%A0-%EB%95%8C-tar-ENOENT-no-such-file-or-directory-lstat-%EC%97%90%EB%9F%AC%EB%82%A0-%EA%B2%BD%EC%9A%B0" rel="nofollow">https://velog.io/@hn04147/npm-install-%ED%95%A0-%EB%95%8C-tar-ENOENT-no-such-file-or-directory-lstat-%EC%97%90%EB%9F%AC%EB%82%A0-%EA%B2%BD%EC%9A%B0</a></p>\n<p dir="auto"><a href="https://m.blog.naver.com/PostView.naver?isHttpsRedirect=true&amp;blogId=chandong83&amp;logNo=221064506346" rel="nofollow">https://m.blog.naver.com/PostView.naver?isHttpsRedirect=true&amp;blogId=chandong83&amp;logNo=221064506346</a></p>\n<p dir="auto"><a href="https://webisfree.com/2021-07-15/npm-install-%EC%97%90%EB%9F%AC-%EB%B0%9C%EC%83%9D-rename-no-such-file-or-directory-%ED%95%B4%EA%B2%B0%ED%95%98%EA%B0%80" rel="nofollow">https://webisfree.com/2021-07-15/npm-install-%EC%97%90%EB%9F%AC-%EB%B0%9C%EC%83%9D-rename-no-such-file-or-directory-%ED%95%B4%EA%B2%B0%ED%95%98%EA%B0%80</a></p>\n<p dir="auto"><a href="https://hellowworlds.tistory.com/57" rel="nofollow">https://hellowworlds.tistory.com/57</a></p>',
+      answer: {},
+      bodyHTML: userStory,
       avatarUrl:
-        "https://avatars.githubusercontent.com/u/97888923?s=64&u=12b18768cdeebcf358b70051283a3ef57be6a20f&v=4",
+        'https://avatars.githubusercontent.com/u/97888923?s=64&u=12b18768cdeebcf358b70051283a3ef57be6a20f&v=4',
     }
   
     let localData = getDataLocalStorage('agoraData')
@@ -102,11 +107,30 @@ const createAgoraDiscussion = () => {
   }
 }
 
-if (localStorage.getItem('agoraData')) {
+const likesEventHandler = (event) => {  
+  let target = event.target
+  if (target.classList.contains('checked')) {
+    target.classList.remove('checked')
+    target.textContent = parseInt(target.textContent) - 1
+  } else if (!target.classList.contains('checked')) {
+    target.classList.add('checked')
+    target.textContent = parseInt(target.textContent) + 1
+  }
+}
+
+// 브라우저가 처음 실행되면,
+// LocalStorage에 저장된 내용이 없으면 Dummy Data를 저장합니다. 
+if (!localStorage.getItem('agoraData')) {
   saveDataLocalStorage(agoraStatesDiscussions)
 }
 
+// LocalStorage에 있는 내용을 HTML에 그려줍니다. 
 render(ul);
 
-submitBtn.addEventListener('click', createAgoraDiscussion)
+// Submit button EventHandler
+form.addEventListener('submit', createAgoraDiscussion)
+
+const likesBtn = document.querySelector('.discussion__likes')
+// Likes button EventHandler
+likesBtn.addEventListener('click', likesEventHandler)
 
